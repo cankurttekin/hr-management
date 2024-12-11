255e69e427457ce3b7b96f9f1d3fa86a78172c62
can kurttekin <cankurttekin@gmail.com>
2024-12-11 15:36:08 +0300

    create candidate controller

---
+++ backend/src/main/java/com/can/kurttekin/hr_management/presentation/rest/CandidateController.java
@@ -0,0 +1,51 @@
+package com.can.kurttekin.hr_management.presentation.rest;
+
+import com.can.kurttekin.hr_management.application.dto.CandidateDto;
+import com.can.kurttekin.hr_management.domain.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/candidates")
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @PostMapping
+    public ResponseEntity<CandidateDto> createCandidate(
+            @RequestBody CandidateDto candidateDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(candidateService.createCandidate(candidateDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CandidateDto>> getAllCandidates() {
+        return ResponseEntity
+                .ok(candidateService.getAllCandidates());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CandidateDto> getCandidateById(@PathVariable Long id) {
+        return ResponseEntity
+                .ok(candidateService.getCandidateById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CandidateDto> updateCandidate(
+            @PathVariable Long id,
+            @RequestBody CandidateDto candidateDto) {
+        return ResponseEntity.ok(candidateService.updateCandidate(id, candidateDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCandidate(@PathVariable Long id) {
+        candidateService.deleteCandidate(id);
+        return ResponseEntity.noContent().build();
+    }
+}
