b2ae2288e4513f20fe4c7a2ef298323780126345
can kurttekin <cankurttekin@gmail.com>
2024-12-11 14:48:14 +0300

    create candidate dto

---
+++ backend/src/main/java/com/can/kurttekin/hr_management/application/dto/CandidateDto.java
@@ -0,0 +1,16 @@
+package com.can.kurttekin.hr_management.application.dto;
+
+import lombok.Data;
+
+@Data
+public class CandidateDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String militaryStatus;
+    private String noticePeriod;
+    private String phone;
+    private String email;
+    private String cv;
+}
