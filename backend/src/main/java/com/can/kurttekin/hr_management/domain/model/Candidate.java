e681bd360109ddf15a5b82b105eb2c806997c873
can kurttekin <cankurttekin@gmail.com>
2024-12-11 14:38:08 +0300

    create candidateentity

---
+++ backend/src/main/java/com/can/kurttekin/hr_management/domain/model/Candidate.java
@@ -0,0 +1,42 @@
+package com.can.kurttekin.hr_management.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "candidates")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "position", nullable = false)
+    private String position;
+
+    @Column(name = "military_status", nullable = false)
+    private String militaryStatus;
+
+    @Column(name = "notice_period", nullable = false)
+    private String noticePeriod;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "cv", nullable = false)
+    private String cv;
+}
